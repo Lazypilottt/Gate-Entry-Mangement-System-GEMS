a9a7da94619098afc8dbe152c41bad3342dccb1d
Aditya <adityay432@gmail.com>
2024-11-13 04:19:18 +0530

    Initial basic structure with entry page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class GateEntryManagementSystem extends React.Component {
+  render() {
+      return (
+        <center>
+          <div className="container">
+              <header className="header">
+                  <h1>Gate Entry Management System - GEMS</h1>
+              </header>
+              
+              <nav className="navbar">
+                  <span>Home Page</span>
+                  <span>Profiles</span>
+                  <span>Entry/Exit</span>
+                  <span>Log Out</span>
+              </nav>
+              
+              <section className="filter-section">
+                  <label>
+                      Name:
+                      <input type="text" />
+                  </label>
+                  <label>
+                      Roll No:
+                      <input type="text" />
+                  </label>
+                  <label>
+                      Category:
+                      <input type="text" />
+                  </label>
+                  <label>
+                      Date of Arrival:
+                      <input type="text" />
+                  </label>
+              </section>
+              
+              <table className="table">
+                  <thead>
+                      <tr>
+                          <th>Name</th>
+                          <th>Roll No</th>
+                          <th>Category</th>
+                          <th>Date and Time of Arrival</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      <tr>
+                          <td>Agastya Nath</td>
+                          <td>12340140</td>
+                          <td>Student</td>
+                          <td>12/11/2024, 11:30 AM</td>
+                      </tr>
+                      {/* Additional rows can be added here */}
+                  </tbody>
+              </table>
+          </div>
+          </center>
+      );
+  }
 }
+export default GateEntryManagementSystem;
 
-export default App;
+ReactDOM.render(<GateEntryManagementSystem />, document.getElementById('root'));
