0b7e90fc5605d5286d2d38427e46985e44861f9a
AdityaJha <adityaj@iitbhilai.ac.in>
2024-11-18 16:25:31 +0530

    Added visiotors page

---
+++ src/components/pages/visitors.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function Visitors() {
+    return (
+        <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+            <h1>Welcome to the Visitors Page</h1>
+            <p>This page contains information about campus visitors.</p>
+        </div>
+    );
+}
+
+export default Visitors;
