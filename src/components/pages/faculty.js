440a42f5369b3248e1182fa0e96f3296ac9fcc3e
AdityaJha <adityaj@iitbhilai.ac.in>
2024-11-18 16:26:04 +0530

    Added faculty page

---
+++ src/components/pages/faculty.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function Faculty() {
+    return (
+        <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+            <h1>Welcome to the Faculty Page</h1>
+            <p>This page contains information about faculty members.</p>
+        </div>
+    );
+}
+
+export default Faculty;
