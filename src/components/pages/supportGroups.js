08720cd67927ec81aa18c382e163dd5f09cae160
AdityaJha <adityaj@iitbhilai.ac.in>
2024-11-18 16:26:38 +0530

    Added support group page

---
+++ src/components/pages/supportGroups.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function SupportGroups() {
+    return (
+        <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+            <h1>Welcome to the Support Groups Page</h1>
+            <p>This page contains information about private support groups.</p>
+        </div>
+    );
+}
+
+export default SupportGroups;
